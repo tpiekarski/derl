8b5cb81191771b1a3bb6e4d545e4a9cef290aed1
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-05-04 08:05:16 +0200

    Iterating over files and reading contents in search_urls

--- src/derl/searcher.py
+++ src/derl/searcher.py
@@ -8,4 +8,14 @@ _logger = logging.getLogger(__name__)
 def search_urls(files):
     _logger.info("Starting to search for URLs in %i files...", len(files))
 
-    # todo: implement searching for URLs
+    if len(files) == 0:
+        _logger.info("No files are available for searching.")
+        return
+
+    for current_entry in files:
+        with open(current_entry, "r") as current_file:
+            lines = current_file.readlines()
+            for current_line in lines:
+                _logger.debug(current_line)
+
+                # todo: implement searching for URLs
