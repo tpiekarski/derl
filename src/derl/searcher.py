36fbebe3fa991eaa4b9d13fef9a755100f9842ca
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-05-04 09:06:40 +0200

    Moving processing functions to processor module

--- src/derl/searcher.py
+++ src/derl/searcher.py
@@ -1,45 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import re
+
+from derl.processor import process_file
 
 _logger = logging.getLogger(__name__)
-_pattern = re.compile(r"^(http|https):\/\/.*$", re.IGNORECASE)
-
-
-def process_file(file):
-    _logger.debug("Spliting current file %s into lines...", file.name)
-    lines = file.readlines()
-
-    if len(lines) == 0:
-        _logger.debug("No lines found, skipping file '%s'", file.name)
-        return
-
-    _logger.debug("Found %i lines", len(lines))
-
-    for current_line in lines:
-        process_line(file, current_line)
-
-
-def process_line(file, line):
-    _logger.debug("Splitting current line into tokens...")
-    tokens = line.split()
-
-    if len(tokens) == 0:
-        _logger.debug("No tokens found, skipping line")
-        return
-
-    _logger.debug("Found %i tokens", len(tokens))
-
-    for current_token in tokens:
-        process_token(file, current_token)
-
-
-def process_token(file, token):
-    match = _pattern.match(token)
-
-    if match:
-        _logger.info("Found a match (%s) in file '%s'", match.string, file.name)
 
 
 def search_urls(files):
