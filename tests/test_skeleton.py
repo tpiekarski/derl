1dfe5069fe035ca43933f8f5ca22bff0af978368
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-06-01 09:38:35 +0200

    Moving from plain assert to unit testing and introducing functional test for main

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from derl.skeleton import check_directory, process_directory, parse_args
+from unittest import TestCase
+from unittest.mock import patch
+from io import StringIO
+
+from derl.skeleton import check_directory, main, process_directory, parse_args
 
 __author__ = "Thomas Piekarski"
 __copyright__ = "Thomas Piekarski"
@@ -9,18 +13,24 @@ __license__ = "mit"
 _TEST_DIRECTORY = "tests/test-directory"
 
 
-def test_parse_directory():
-    raw_args = [_TEST_DIRECTORY]
-    parsed_args = parse_args(raw_args)
+class SkeletonTest(TestCase):
 
-    assert parsed_args.directory == _TEST_DIRECTORY
+    def test_parse_arguments(self):
+        raw_args = [_TEST_DIRECTORY]
+        parsed_args = parse_args(raw_args)
 
+        self.assertEqual(parsed_args.directory, _TEST_DIRECTORY)
 
-def test_check_directory():
-    assert check_directory(_TEST_DIRECTORY)
-    assert not check_directory("not-existing-directory")
+    def test_check_directory(self):
+        self.assertTrue(check_directory(_TEST_DIRECTORY))
+        self.assertFalse(check_directory("not-existing-directory"))
 
+    def test_process_directory(self):
+        self.assertEqual(len(process_directory(_TEST_DIRECTORY, [])), 4)
+        self.assertEqual(len(process_directory("not-existing-directory", [])), 0)
 
-def test_process_directory():
-    assert len(process_directory(_TEST_DIRECTORY, [])) == 4
-    assert len(process_directory("not-existing-directory", [])) == 0
+    def test_main(self):
+        with patch("sys.stdout", new=StringIO()) as fake_stdout:
+            with open("tests/reference-output.log", "r") as reference:
+                main([_TEST_DIRECTORY])
+                self.assertEqual(fake_stdout.getvalue(), reference.read())
