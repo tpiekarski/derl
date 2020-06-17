814d251c63bf84026ea7f33a45089592e0b9c07f
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-06-17 13:22:06 +0200

    Customizing display of argparse help

--- src/derl/parser.py
+++ src/derl/parser.py
@@ -4,7 +4,7 @@
 # Copyright 2020 Thomas Piekarski <t.piekarski@deloquencia.de>
 #
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, HelpFormatter
 from logging import DEBUG, INFO
 
 from derl import __version__
@@ -12,24 +12,31 @@ from derl.dispatcher import _DEFAULT_RETRY, _DEFAULT_TIMEOUT
 
 
 def parse_args(args):
-    parser = ArgumentParser(description="Dead URL searching utility")
+    parser = ArgumentParser(
+        prog="derl",
+        formatter_class=lambda prog: HelpFormatter(prog, max_help_position=35, width=90),
+        description="Dead URL searching utility")
+
     parser.add_argument(
         action="store",  # default behavior
         dest="directory",
         help="directory for looking for dead URLs",
         metavar="directory")
+
     parser.add_argument(
         "-c",
         "--context",
         action="store_true",
         dest="context",
         help="showing up to 3 lines of context")
+
     parser.add_argument(
         "-d",
         "--dispatch",
         action="store_true",
         dest="dispatch",
         help="dispatching HTTP requests for every found URL")
+
     parser.add_argument(
         "-r",
         "--retry",
@@ -38,6 +45,7 @@ def parse_args(args):
         dest="retry",
         help="set how often to retry a request (default is 3)",
         type=int)
+
     parser.add_argument(
         "-t",
         "--timeout",
@@ -46,10 +54,12 @@ def parse_args(args):
         dest="timeout",
         help="set timeout for requests in seconds (default is 10)",
         type=int)
+
     parser.add_argument(
         "--version",
         action="version",
         version="derl {ver}".format(ver=__version__))
+
     parser.add_argument(
         "-v",
         "--verbose",
@@ -57,6 +67,7 @@ def parse_args(args):
         const=INFO,
         dest="loglevel",
         help="set loglevel to INFO",)
+
     parser.add_argument(
         "-vv",
         "--very-verbose",
