4fe7a16801c41b5bd796db15c742e693b97bbda7
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-07-02 08:40:22 +0200

    Solving last little issue reported by SonarCloud

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,8 @@
 """
     Dummy conftest.py for derl.
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    https://pytest.org/latest/plugins.html
+    Read more about conftest.py under: https://pytest.org/latest/plugins.html
 """
-
-# import pytest
 
 _TEST_DIRECTORY = "tests/test-directory"
 _TEST_REQUEST_RETRIES = 6
