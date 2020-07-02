423c75c86350317dcefd62b822dff5bf814c9e19
Thomas Piekarski <t.piekarski@deloquencia.de>
2020-07-02 15:54:05 +0200

    Shorten stats class variables

--- src/derl/model/stats.py
+++ src/derl/model/stats.py
@@ -6,23 +6,23 @@
 
 
 class Stats():
-    directories_processed = 0
-    files_processed = 0
-    lines_processed = 0
-    requests_sent = 0
-    tokens_processed = 0
-    urls_processed = 0
+    directories = 0
+    files = 0
+    lines = 0
+    requests = 0
+    tokens = 0
+    urls = 0
 
     def __str__(self):
         output = ""
 
         output += "---\n"
-        output += "Directories:\t{0}\n".format(self.directories_processed)
-        output += "Files:\t\t{0}\n".format(self.files_processed)
-        output += "Lines:\t\t{0}\n".format(self.lines_processed)
-        output += "Tokens:\t\t{0}\n".format(self.tokens_processed)
-        output += "URLs:\t\t{0}\n".format(self.urls_processed)
-        output += "Requests:\t{0}\n".format(self.requests_sent)
+        output += "Directories:\t{0}\n".format(self.directories)
+        output += "Files:\t\t{0}\n".format(self.files)
+        output += "Lines:\t\t{0}\n".format(self.lines)
+        output += "Tokens:\t\t{0}\n".format(self.tokens)
+        output += "URLs:\t\t{0}\n".format(self.urls)
+        output += "Requests:\t{0}\n".format(self.requests)
 
         return output
 
@@ -30,19 +30,19 @@ class Stats():
         return self.__str__()
 
     def inc_directories(self):
-        self.directories_processed += 1
+        self.directories += 1
 
     def inc_files(self):
-        self.files_processed += 1
+        self.files += 1
 
     def inc_lines(self):
-        self.lines_processed += 1
+        self.lines += 1
 
     def inc_tokens(self):
-        self.tokens_processed += 1
+        self.tokens += 1
 
     def inc_urls(self):
-        self.urls_processed += 1
+        self.urls += 1
 
     def inc_requests(self):
-        self.requests_sent += 1
+        self.requests += 1
